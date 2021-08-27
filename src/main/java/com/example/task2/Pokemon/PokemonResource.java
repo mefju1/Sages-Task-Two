14c2fecf35960109f9ba9f168b69d2a962fadccf
Mateusz Somionka <mateuszsomionka@wp.pl>
2021-08-27 19:48:04 +0200

    Cannot deserialize value of type. time's up. Thank you

--- src/main/java/com/example/task2/Pokemon/PokemonResource.java
+++ src/main/java/com/example/task2/Pokemon/PokemonResource.java
@@ -1,12 +1,18 @@
 package com.example.task2.Pokemon;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -18,11 +24,12 @@ public class PokemonResource {
     private final RestTemplate restTemplate;
 
     @GetMapping("/{firstPokemon}/{secondPokemon}")
-    public ResponseEntity<Pokemon> getPokemonTypes(@PathVariable("firstPokemon") String firstPokemon,
-                                                   @PathVariable("secondPokemon") String lastPokemon) {
+    public List<Object> getPokemonTypes(@PathVariable("firstPokemon") String firstPokemon,
+                                        @PathVariable("secondPokemon") String lastPokemon) {
 
-        Pokemon pokemon = restTemplate.getForObject("https://pokeapi.co/api/v2/" + firstPokemon, Pokemon.class);
-
-        return new ResponseEntity<>(pokemon, OK);
+        Object[] pokemons = restTemplate.getForObject("https://pokeapi.co/api/v2/type/" + firstPokemon, Object[].class);
+        Object[]  pokemon2 = restTemplate.getForObject("https://pokeapi.co/api/v2/type/" + lastPokemon, Object[].class);
+        
+        return Arrays.asList(pokemons);
     }
 }
