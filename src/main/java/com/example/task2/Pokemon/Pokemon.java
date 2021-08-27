7a1d0c78b8433a6bda44aaf22b69a22efa8707c9
Mateusz Somionka <mateuszsomionka@wp.pl>
2021-08-27 19:25:47 +0200

    Changed WebClient to RestTemplate, but still not woring :(

--- src/main/java/com/example/task2/Pokemon/Pokemon.java
+++ src/main/java/com/example/task2/Pokemon/Pokemon.java
@@ -2,11 +2,13 @@ package com.example.task2.Pokemon;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
+@ToString
 @Getter
 @Setter
 public class Pokemon {
-    private List<String> types;
+    private List<String> name;
 }
--- src/main/java/com/example/task2/Pokemon/PokemonResource.java
+++ src/main/java/com/example/task2/Pokemon/PokemonResource.java
@@ -1,22 +1,28 @@
 package com.example.task2.Pokemon;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/pokemon/fight")
 public class PokemonResource {
+
+    private final RestTemplate restTemplate;
 
     @GetMapping("/{firstPokemon}/{secondPokemon}")
     public ResponseEntity<Pokemon> getPokemonTypes(@PathVariable("firstPokemon") String firstPokemon,
                                                    @PathVariable("secondPokemon") String lastPokemon) {
 
-        return new ResponseEntity<>(new Pokemon(), OK);
+        Pokemon pokemon = restTemplate.getForObject("https://pokeapi.co/api/v2/" + firstPokemon, Pokemon.class);
+
+        return new ResponseEntity<>(pokemon, OK);
     }
 }
