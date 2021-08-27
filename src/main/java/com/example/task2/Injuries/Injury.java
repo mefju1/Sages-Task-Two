7c76676660419e2c4b45dee9b092dcf156e1972b
Mateusz Somionka <mateuszsomionka@wp.pl>
2021-08-27 18:54:38 +0200

    Added classes Pokemon, Injuries

---
+++ src/main/java/com/example/task2/Injuries/Injury.java
@@ -0,0 +1,10 @@
+package com.example.task2.Injuries;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Injury {
+    private Long damage;
+}
---
+++ src/main/java/com/example/task2/Pokemon/Pokemon.java
@@ -0,0 +1,12 @@
+package com.example.task2.Pokemon;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Pokemon {
+    private List<String> types;
+}
